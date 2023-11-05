bfa0815a49d1d33a4b921e15c48f400c699a6590
westcode6 <westcode6@gmail.com>
2023-11-05 01:01:13 +0100

    very good

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
             <Navbar />
 
             <div className="relative z-0">
-            <StarsCanvas />
+            {/* <StarsCanvas /> */}
 
             <Hero />
             </div>
